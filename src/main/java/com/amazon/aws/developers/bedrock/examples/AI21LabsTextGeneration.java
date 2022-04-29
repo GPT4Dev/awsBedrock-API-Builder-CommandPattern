665f54786d4ee3ab0d3f6506727d734f3951e611
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-04-29 19:35:22 +0000

    Update localization files

--- src/main/java/com/amazon/aws/developers/bedrock/examples/AI21LabsTextGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/AI21LabsTextGeneration.java
@@ -18,3 +18,34 @@ public class AI21LabsTextGeneration {
 
     private static final String PROMPT = """
         Extract the band name from the contract:
+
+        This Music Recording Agreement ("Agreement") is made effective as of the 13 day of December,
+        2021 by and between Good Kid, a Toronto-based musical group (“Artist”) and Universal Music Group,
+        a record label with license number 545345 (“Recording Label"). Artist and Recording Label may each
+        be referred to in this Agreement individually as a "Party" and collectively as the "Parties."
+        Work under this Agreement shall begin on March 15, 2022.
+    """;
+
+    public static void main(String... args) throws Exception {
+
+        try (BedrockRuntimeClient bedrockClient = BedrockRuntimeClient.builder()
+            .region(Region.US_EAST_1)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build()) {
+
+            String bedrockBody = BedrockRequestBody.builder()
+                .withModelId(MODEL_ID)
+                .withPrompt(PROMPT)
+                .build();
+
+            InvokeModelRequest invokeModelRequest = InvokeModelRequest.builder()
+                .modelId(MODEL_ID)
+                .body(SdkBytes.fromString(bedrockBody, Charset.defaultCharset()))
+                .build();
+
+            InvokeModelResponse invokeModelResponse = bedrockClient.invokeModel(invokeModelRequest);
+            JSONObject responseAsJson = new JSONObject(invokeModelResponse.body().asUtf8String());
+
+            System.out.println("🤖 Response: ");
+            System.out.println(responseAsJson
+                .getJSONArray("completions")
