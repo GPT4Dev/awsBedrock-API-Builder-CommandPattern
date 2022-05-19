c850bfb18561eefda43e2e3af4971ee92dd736eb
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-05-19 12:30:02 +0000

    Create app.py

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -11,4 +11,5 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeAsyncClient;
-import software.amazon.awssdk.services.bedrockruntime.model.Invo
+import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelWithResponseStreamRequest;
+import software
