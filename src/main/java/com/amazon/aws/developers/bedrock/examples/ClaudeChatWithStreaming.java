db0bf0aa70c15d3f6a7d01dfb75178c78d108690
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-06-11 13:12:48 +0000

    debug off

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -22,3 +22,4 @@ public class ClaudeChatWithStreaming {
     private static final String PROMPT = """
         Generative AI refers to artificial intelligence systems that are capable of generating
         novel content such as text, images, audio, video, and more, as opposed to just analyzing
+        existing data. The key 
