2d5b645873b04e14929bc42ea134baa97333cb50
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-07-02 20:33:39 +0000

    -m SUBUTAI-1347

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -28,4 +28,5 @@ public class ClaudeChatWithStreaming {
         datasets to build an understanding of patterns and structures within the data. It then
         uses that knowledge to generate new artifacts that conform to those patterns but are not
         identical copies, allowing for creativity and imagination. Prominent examples of generative
-        AI include systems like DALL-E that creates images from tex
+        AI include systems like DALL-E that creates images from text descriptions, GPT-3 that
+        gene
