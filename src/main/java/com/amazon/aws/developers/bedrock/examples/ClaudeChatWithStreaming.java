d46867cdee09e345363812152a79032acaee80b9
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-07-23 05:56:54 +0000

    fix style errors

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -32,4 +32,5 @@ public class ClaudeChatWithStreaming {
         generates human-like text, and WaveNet that produces realistic synthetic voices. Generative
         models hold great promise for assisting and augmenting human creativity across many domains
         but also raise concerns about potential misuse if not thoughtfully implemented. Overall,
-        generative AI aims to mimic human creative abilities
+        generative AI aims to mimic human creative abilities at scale to autonomously produce
+        high
