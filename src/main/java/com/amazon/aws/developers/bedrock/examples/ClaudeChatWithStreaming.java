08b502a9ce16b21f5aebe7b0ff265bf4ac150a16
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-08-28 06:39:04 +0000

    Improve Documentation

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -55,3 +55,4 @@ public class ClaudeChatWithStreaming {
                             .withModelId(MODEL_ID)
                             .withPrompt(PROMPT)
                             .withInferenceParameter("max_tokens_to_sample", 2048)
+                            .withInfer
