8ea96c8c86187a9b497792cd626fcf4dd15b1c5b
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-10-22 13:06:25 +0000

    Latest Commit from sublime text

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -72,4 +72,4 @@ public class ClaudeChatWithStreaming {
                                         System.out.println("🤖 Response: ");
                                     })
                                     .subscriber(eventConsumer -> {
-                                        eventConsumer.accept(new 
+                                        eventConsumer.accept(new InvokeModelWithResponseStre
