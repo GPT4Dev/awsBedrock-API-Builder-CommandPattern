214bc17e4f8f98a7d89e0353d86659579cb03ff2
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-11-15 18:07:12 +0000

    footer delete

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -80,4 +80,4 @@ public class ClaudeChatWithStreaming {
                                             }
                                         });
                                     })
-           
+                                    .onComple
