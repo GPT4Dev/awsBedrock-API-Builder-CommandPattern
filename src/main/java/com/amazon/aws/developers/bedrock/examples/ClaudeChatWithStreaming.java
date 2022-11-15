c8517106ccef0cba07e7ee988741debd7b9ad8c8
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-11-15 23:02:23 +0000

    - Updating nuget spec

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeChatWithStreaming.java
@@ -80,4 +80,5 @@ public class ClaudeChatWithStreaming {
                                             }
                                         });
                                     })
-                                    .onComple
+                                    .onComplete(() -> {
+                         
