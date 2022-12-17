164faebead7db591d9e0878ac88df4a6ef21b9ce
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-12-17 17:10:28 +0000

    Bumped package version.

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -2,4 +2,4 @@ package com.amazon.aws.developers.bedrock.examples;
 
 import java.nio.charset.Charset;
 import org.json.JSONObject;
-import com.amazon.aws.developer
+import com.amazon.aws.developers.bedrock.util.BedrockRequestBody;
