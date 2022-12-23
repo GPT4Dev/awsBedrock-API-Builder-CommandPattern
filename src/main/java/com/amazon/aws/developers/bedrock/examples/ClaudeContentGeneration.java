50c107ebd4e69c38083a3a32b7846b5f92d56bc7
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2022-12-23 05:49:39 +0000

    update makefile

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -3,3 +3,5 @@ package com.amazon.aws.developers.bedrock.examples;
 import java.nio.charset.Charset;
 import org.json.JSONObject;
 import com.amazon.aws.developers.bedrock.util.BedrockRequestBody;
+
+import software.amazon.awssdk.aut
