f402d5f86e132f9c4e904744d1d9122cf4b5b736
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-01-01 22:36:40 +0000

    xcode project update

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -8,4 +8,5 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
-import software.amazon.awssdk.services.bedrockruntime.model.
+import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
+import 
