9097eeed2acc972934291afa417158de426ddb7a
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-01-30 15:21:27 +0000

    notebook updates

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -18,4 +18,5 @@ public class ClaudeContentGeneration {
     private static final String PROMPT = """
         Generative AI refers to artificial intelligence systems that are capable of generating
         novel content such as text, images, audio, video, and more, as opposed to just analyzing
-        existing data. The key aspect of generative AI is that it creates brand new o
+        existing data. The key aspect of generative AI is that it creates brand new outputs that
+        are uniq
