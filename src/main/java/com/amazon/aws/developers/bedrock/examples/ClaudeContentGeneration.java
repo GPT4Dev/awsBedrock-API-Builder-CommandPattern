5f7973d9885d8c429d9c8ab468dfbe74f78d30a4
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-02-05 05:40:02 +0000

    Release v0.5.4

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -21,4 +21,5 @@ public class ClaudeContentGeneration {
         existing data. The key aspect of generative AI is that it creates brand new outputs that
         are unique and original, not just reproductions or remixes of existing content. Generative
         AI leverages machine learning techniques like neural networks that are trained on large
-        datasets to build an understanding of patterns and structures wit
+        datasets to build an understanding of patterns and structures within the data. It then
+        u
