42f2b234344a655c762b1bf9c0afa45261b35385
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-02-10 17:01:13 +0000

    added defaults

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -24,3 +24,4 @@ public class ClaudeContentGeneration {
         datasets to build an understanding of patterns and structures within the data. It then
         uses that knowledge to generate new artifacts that conform to those patterns but are not
         identical copies, allowing for creativity and imagination. Prominent examples of generative
+        AI include systems like DALL-E 
