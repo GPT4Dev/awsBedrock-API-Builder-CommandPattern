a78d6eed91ea889cf50ea8230e94ff139085d0e9
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-02-25 08:00:21 +0000

    Update catalogue

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -30,3 +30,5 @@ public class ClaudeContentGeneration {
         but also raise concerns about potential misuse if not thoughtfully implemented. Overall,
         generative AI aims to mimic human creative abilities at scale to autonomously produce
         high-quality, diverse, and novel content.        
+
+        Please create a single
