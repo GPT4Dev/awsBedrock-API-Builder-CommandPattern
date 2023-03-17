ba1f119aefbe639fb2a133916629f2e29aeee78f
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-03-17 05:57:41 +0000

    Bump to 0.1.1

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -48,4 +48,6 @@ public class ClaudeContentGeneration {
                 .withInferenceParameter("temperature", 0.5)
                 .withInferenceParameter("top_k", 250)
                 .withInferenceParameter("top_p", 1)
- 
+                .build();
+
+    
