6ac597ebcf1ec6a391c72277d87c1fce5afb7f65
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-03-29 22:38:05 +0000

    --ammend

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -58,4 +58,5 @@ public class ClaudeContentGeneration {
             InvokeModelResponse invokeModelResponse = bedrockClient.invokeModel(invokeModelRequest);
             JSONObject responseAsJson = new JSONObject(invokeModelResponse.body().asUtf8String());
 
-            System.ou
+            System.out.println("🤖 Response: ");
+          
