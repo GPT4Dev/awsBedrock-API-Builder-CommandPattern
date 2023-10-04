8e7f283888e37e5b1603a1b7daf2678c04614789
GPT4Dev <136254312+GPT4Dev@users.noreply.github.com>
2023-10-04 20:59:18 +0000

    updated changelog and version

--- src/main/java/com/amazon/aws/developers/bedrock/util/BedrockRequestBody.java
+++ src/main/java/com/amazon/aws/developers/bedrock/util/BedrockRequestBody.java
@@ -201,4 +201,4 @@ class AnthropicCommand extends BedrockBodyCommand {
 
 class CohereCommand extends BedrockBodyCommand {
 
-    public CohereCommand(String prompt, M
+    public CohereCommand(String prompt, Map<String, Object> inferenceParameters
